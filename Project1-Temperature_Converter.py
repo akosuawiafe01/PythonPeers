ebd81d3161ec2acc9785fe01fb57425b27fcefb5
akosuawiafe01 <wiafenyarkoa@gmail.com>
2020-03-28 01:34:31 +0000

    Fahrenheit to Celcius Py Calculator

---
+++ Project1-Temperature_Converter.py
@@ -0,0 +1,14 @@
+#Temperature Converter: Fahrenheit to Celcius
+
+print("\n Hey there, welcome to the FC Temperature Calculator \n")
+
+print('Please enter your tempearture in Farenheit: \n')
+temperature = input()
+
+
+fahrenheit = int(temperature) - 32
+
+celcius = int(fahrenheit) / 1.8
+
+
+print('Your temperature is ', str(round(celcius,2)) , ' degree celcius')
