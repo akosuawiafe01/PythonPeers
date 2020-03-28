9e9184a5f6a0cfdd0174920eefce536d27df90d6
akosuawiafe01 <wiafenyarkoa@gmail.com>
2020-03-28 01:35:12 +0000

    COVID-19 Symptoms Collector

---
+++ Project2-COVID-19_Interactive_Screen.py
@@ -0,0 +1,30 @@
+#This program collects the symptoms of patients who are to test for the COVID-19 disease
+
+print("Welcome to the COVID-19 Interactive Screen ")
+
+
+print("Enter your name: \n")
+name = input()
+
+print("Enter your age: \n")
+age = input()
+
+print("Enter your place of residence: \n")
+residence = input()
+
+print("Enter your symptoms: \n")
+symptoms = input()
+
+
+#Patient validates the information entered above
+
+print ("You entered the following details: \n")
+
+print ('Name: ' + name + '\n')
+print ('Age: ' + age + '\n')
+print ('Place of Residence: ' + residence + '\n')
+print ('Symptoms: ' + symptoms + '\n')
+
+
+
+print("\t\t ********** Your details have been saved successfully, Thank you **********")
